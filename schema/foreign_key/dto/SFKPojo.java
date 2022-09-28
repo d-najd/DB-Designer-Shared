14c90592b7acf7e5bb67770c25d048dba5d4c98a
Deltashot <dimitar.hristijan.najdovski@gmail.com>
2022-09-28 16:35:18 +0200

    updating changes from the api

--- schema/foreign_key/dto/SFKPojo.java
+++ schema/foreign_key/dto/SFKPojo.java
@@ -1,25 +1,26 @@
 package com.umldesigner.submodules.UmlDesignerShared.schema.foreign_key.dto;
 
-import com.umldesigner.submodules.UmlDesignerShared.infrastructure.pojo.pojos.BaseMPojo;
-
+import com.umldesigner.submodules.UmlDesignerShared.infrastructure.pojo.pojos.BasePojo;
 import com.umldesigner.submodules.UmlDesignerShared.infrastructure.pojo.utils.MyCloneable;
+import com.umldesigner.submodules.UmlDesignerShared.schema.item_info.SItemInfoPojo;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@Data
-@EqualsAndHashCode(callSuper = true)
 /**
  * @apiNote value1 and value2 in the identity refer to id's of SItems
  */
-public class SFKPojo extends BaseMPojo implements MyCloneable<SFKPojo> {
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class SFKPojo extends BasePojo implements MyCloneable<SFKPojo> {
 
-    protected String firstTableUuid;
-    
-    protected String secondTableUuid;
+    protected String referencedUuid;
 
     protected String onDelete = "ca";
 
     protected String onUpdate = "ca";
+
+    // protected SItemInfoPojo itemInfo;
 
     @Override
     public SFKPojo clone() {
